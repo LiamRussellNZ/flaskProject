e85d4e9d4263b1a08a2ed8f7a6c82c3b639c21a6
Liam <liam@enuffsaid.co.nz>
2021-03-11 21:24:20 +1300

    Following flask.palletsproject tutorial

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def helloWorld():
     return 'Hello, World! Hoory this works'
+
+@app.route('/hello/', methods = ['GET'])
+def helloUser():
+    name = request.args.get("name", None)
+    return f'Hello, {name}'
