efaeeeaf343eb5f6cf630008886df9c576f8e1f9
Liam <liam@enuffsaid.co.nz>
2021-03-25 19:56:02 +1300

    Starting a simple rest api following Python REST API Tutorial - Building a Flask REST API on youtube

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
+api = Api(app)
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
+video_put_args.add_argument("likes", type=int, help="Like on the video is required", required=True)
+
+videos = {}
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 @app.route('/')
 def helloWorld():
